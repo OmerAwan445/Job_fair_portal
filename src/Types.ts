a18727316ad1a4a1836d8f3f8be92b2b87fecd3b
Hasnat Ahmed Goheer <ch.hsyahmedgoheer@hotmail.com>
2023-07-07 02:57:54 +0500

    Intital Setup of sending data to the server

---
+++ src/Types.ts
@@ -0,0 +1,9 @@
+export interface AccountCreationVerification{
+    
+}
+
+
+export interface NewAccountData
+{
+
+}
---
+++ src/utils/AccountCreationVerification.ts
@@ -0,0 +1,7 @@
+import axios from "axios";
+import { NewAccountData,AccountCreationVerification} from "../Types";
+
+export async function createAccount  (newAccountData: NewAccountData)  {
+
+
+}
